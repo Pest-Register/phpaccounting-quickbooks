aacbb15644ff06ccf4f164c20c58209a9c689900
Dylan <dylan@pestregister.com>
2019-10-08 14:22:14 +1100

    added in normal journals

--- src/Message/Journals/Request/GetJournalRequest.php
+++ src/Message/Journals/Request/GetJournalRequest.php
@@ -9,7 +9,98 @@
 namespace PHPAccounting\XERO\Message\Journals\Request;
 
 
-class GetJournalRequest
-{
+use PHPAccounting\Quickbooks\Helpers\ErrorParsingHelper;
+use PHPAccounting\Quickbooks\Message\AbstractRequest;
+use PHPAccounting\XERO\Message\Journals\Response\GetJournalResponse;
 
+class GetJournalRequest extends AbstractRequest
+{
+    /**
+     * Set AccountingID from Parameter Bag (AccountID generic interface)
+     * @see https://developer.intuit.com/app/developer/qbo/docs/api/accounting/all-entities/customer
+     * @param $value
+     * @return GetJournalRequest
+     */
+    public function setAccountingID($value) {
+        return $this->setParameter('accounting_id', $value);
+    }
+
+    /**
+     * Set Page Value for Pagination from Parameter Bag
+     * @see https://developer.intuit.com/app/developer/qbo/docs/api/accounting/all-entities/customer
+     * @param $value
+     * @return GetJournalRequest
+     */
+    public function setPage($value) {
+        return $this->setParameter('page', $value);
+    }
+
+    /**
+     * Accounting ID (AccountID)
+     * @return mixed comma-delimited-string
+     */
+    public function getAccountingID() {
+        if ($this->getParameter('accounting_id')) {
+            return $this->getParameter('accounting_id');
+        }
+        return null;
+    }
+
+    /**
+     * Return Page Value for Pagination
+     * @return integer
+     */
+    public function getPage() {
+        return $this->getParameter('page');
+    }
+
+    /**
+     * Get the raw data array for this message. The format of this varies from gateway to
+     * gateway, but will usually be either an associative array, or a SimpleXMLElement.
+     *
+     * @return mixed
+     * @throws \Omnipay\Common\Exception\InvalidRequestException
+     */
+    public function getData()
+    {
+        $this->validate('accounting_id');
+
+        return $this->data;
+    }
+
+    /**
+     * Send Data to Xero Endpoint and Retrieve Response via Response Interface
+     * @param mixed $data Parameter Bag Variables After Validation
+     * @return GetJournalResponse
+     * @throws \QuickBooksOnline\API\Exception\IdsException
+     */
+    public function sendData($data)
+    {
+        $quickbooks = $this->createQuickbooksDataService();
+
+        if ($this->getAccountingID()) {
+            $accounts = $quickbooks->FindById('journalentry', $this->getAccountingID());
+            $response = $accounts;
+        } else {
+            $response = $quickbooks->FindAll('journalentry', $this->getPage(), 500);
+        }
+
+        $error = $quickbooks->getLastError();
+        if ($error) {
+            $response = ErrorParsingHelper::parseError($error);
+        }
+
+        return $this->createResponse($response);
+    }
+
+
+    /**
+     * Create Generic Response from Xero Endpoint
+     * @param mixed $data Array Elements or Xero Collection from Response
+     * @return GetJournalResponse
+     */
+    public function createResponse($data)
+    {
+        return $this->response = new GetJournalResponse($this, $data);
+    }
 }
--- src/Message/Journals/Response/GetJournalResponse.php
+++ src/Message/Journals/Response/GetJournalResponse.php
@@ -9,7 +9,113 @@
 namespace PHPAccounting\XERO\Message\Journals\Response;
 
 
-class GetJournalResponse
+use PHPAccounting\Quickbooks\Message\Response;
+use QuickBooksOnline\API\Data\IPPJournalEntry;
+
+class GetJournalResponse extends Response
 {
 
+    /**
+     * Check Response for Error or Success
+     * @return boolean
+     */
+    public function isSuccessful()
+    {
+        if (array_key_exists('error', $this->data)) {
+            if ($this->data['error']['status']){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Fetch Error Message from Response
+     * @return string
+     */
+    public function getErrorMessage(){
+        if ($this->data['error']['status']){
+            if (strpos($this->data['error']['detail'], 'Token expired') !== false) {
+                return 'The access token has expired';
+            } else {
+                return $this->data['error']['detail'];
+            }
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Add LineItems to JournalEntry
+     * @param $data
+     * @param $journal
+     * @return mixed
+     */
+    private function parseJournalItems($data, $journal) {
+        if ($data) {
+            $journalItems = [];
+            foreach($data as $journalItem) {
+                $newJournalItem = [];
+                $newJournalItem['tax_amount'] = 0;
+                $newJournalItem['gross_amount'] = 0;
+                $newJournalItem['net_amount'] = 0;
+                $newJournalItem['accounting_id'] = $journalItem->Id;
+                $newJournalItem['description'] = $journalItem->Description;
+                $newJournalItem['credit'] = $journalItem->JournalEntryLineDetail->PostingType == 'Credit'? true : false;
+                $newJournalItem['gross_amount'] = $journalItem->Amount;
+
+                if(isset($journalItem->journalLineDetail->AccountRef)){
+                    $newJournalItem['account_code'] = $journalItem->journalLineDetail->AccountRef->value;
+                    $newJournalItem['account_name'] = $journalItem->journalLineDetail->AccountRef->name;
+                }
+
+                if(isset($journalItem->journalLineDetail->TaxCodeRef)){
+                    $newJournalItem['tax_type'] = $journalItem->journalLineDetail->TaxCodeRef->value;
+                }
+                if(isset($journalItem->journalLineDetail->TaxAmount)){
+                    $newJournalItem['tax_amount'] = $journalItem->journalLineDetail->TaxAmount;
+                    $newJournalItem['net_amount'] = (float) $newJournalItem['tax_amount'] + (float) $newJournalItem['gross_amount'];
+                }
+
+                array_push($journalItems, $newJournalItem);
+            }
+
+            $journal['journal_data'] = $journalItems;
+        }
+        return $journal;
+    }
+
+    /**
+     * Return all JournalEntries with Generic Schema Variable Assignment
+     * @return array
+     */
+    public function getJournals(){
+        $journalEntrys = [];
+        if ($this->data instanceof IPPJournalEntry){
+            $journalEntry = $this->data;
+            $newJournalEntry = [];
+            $newJournalEntry['accounting_id'] = $journalEntry->Id;
+            $newInvoice['sync_token'] = $journalEntry->SyncToken;
+            $newInvoice['date'] = $journalEntry->TxnDate;
+            $newInvoice['updated_at'] = $journalEntry->MetaData['LastUpdatedTime'];
+            $newInvoice = $this->parseJournalItems($journalEntry->Line, $newInvoice);
+
+            array_push($invoices, $newInvoice);
+
+        } else {
+            foreach ($this->data as $journalEntry) {
+                $newJournalEntry = [];
+                $newJournalEntry['accounting_id'] = $journalEntry->Id;
+                $newInvoice['sync_token'] = $journalEntry->SyncToken;
+                $newInvoice['date'] = $journalEntry->TxnDate;
+                $newInvoice['updated_at'] = $journalEntry->MetaData['LastUpdatedTime'];
+                $newInvoice = $this->parseJournalItems($journalEntry->Line, $newInvoice);
+                array_push($invoices, $newInvoice);
+            }
+        }
+
+        return $journalEntrys;
+    }
 }
