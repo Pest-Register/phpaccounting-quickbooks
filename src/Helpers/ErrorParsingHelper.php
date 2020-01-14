c2113ca2bb0cf678471931c327353398f644af57
Max Yendall <yendallmax@gmail.com>
2020-01-14 17:09:03 +1100

    Fix to error parser

--- src/Helpers/ErrorParsingHelper.php
+++ src/Helpers/ErrorParsingHelper.php
@@ -15,9 +15,21 @@ class ErrorParsingHelper
         $message = '';
         if (array_key_exists('Message', $errorObj['Fault']['Error'])) {
             $messageExploded = explode(';', $errorObj['Fault']['Error']['Message']);
-            $message = substr($messageExploded[0], strpos($messageExploded[0], "=") + 1);
-            $errorCode = substr($messageExploded[1], strpos($messageExploded[1], '=') + 1);
-            $statusCode = substr($messageExploded[2], strpos($messageExploded[2], '=') + 1);
+            $message = '';
+            $errorCode = '';
+            $statusCode = '';
+
+            if (array_key_exists(0, $messageExploded)) {
+                $message = substr($messageExploded[0], strpos($messageExploded[0], "=") + 1);
+            }
+
+            if (array_key_exists(1, $messageExploded)) {
+                $errorCode = substr($messageExploded[1], strpos($messageExploded[1], '=') + 1);
+            }
+            if (array_key_exists(2, $messageExploded)) {
+                $statusCode = substr($messageExploded[2], strpos($messageExploded[2], '=') + 1);
+            }
+
             $message = "Message: ".$message. "\nError Code: ". $errorCode. "\nStatus Code: ".$statusCode;
             if (array_key_exists('Detail', $errorObj['Fault']['Error'])) {
                 $detail = $errorObj['Fault']['Error']['Detail'];
