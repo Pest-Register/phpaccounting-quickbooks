7641db44e7e2c89a873237487f8e5a6f184c5270
Max Yendall <yendallmax@gmail.com>
2020-05-22 12:25:49 +1000

    Return array from exceptions

--- src/Helpers/ErrorResponseHelper.php
+++ src/Helpers/ErrorResponseHelper.php
@@ -9,21 +9,21 @@ class ErrorResponseHelper
     /**
      * @param $response
      * @param string $model
-     * @return string
+     * @return array
      */
     public static function parseErrorResponse ($response, $model = '') {
         switch ($model) {
             default:
                 if (strpos($response, 'Duplicate') !== false) {
-                    $response = 'Duplicate model found';
+                    $response = [ 'message' => 'Duplicate model found' ];
                 } else if (strpos($response, 'Existing') !== false) {
-                    $response = 'No model found from given ID';
+                    $response = [ 'message' => 'No model found from given ID' ];
                 } elseif (strpos($response, 'Token expired') !== false || strpos($response, 'AuthenticationFailed') !== false) {
-                    $response = 'The access token has expired';
+                    $response = [ 'message' => 'The access token has expired' ];
                 } elseif (strpos($response, 'Unsupported Operation') !== false) {
-                    $response = 'Model cannot be edited';
+                    $response = ['message' => 'Model cannot be edited' ];
                 } elseif (strpos($response, 'A business validation error has occurred while processing your request') !== false) {
-                    $response = 'Validation exception. Possibly duplicate ID or business error';
+                    $response = [ 'message' => 'Validation exception. Possibly duplicate ID or business error' ];
                 }
                 return $response;
         }
