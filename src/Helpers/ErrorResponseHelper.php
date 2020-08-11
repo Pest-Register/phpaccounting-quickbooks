54a8616c4b8c06e13d0af950caf74f28dfa98deb
Max Yendall <yendallmax@gmail.com>
2020-08-12 09:56:17 +1000

    Added raw exception to error response helper

--- src/Helpers/ErrorResponseHelper.php
+++ src/Helpers/ErrorResponseHelper.php
@@ -15,17 +15,17 @@ class ErrorResponseHelper
         switch ($model) {
             default:
                 if (strpos($response, 'Duplicate') !== false) {
-                    $response = [ 'message' => 'Duplicate model found' ];
+                    $response = [ 'message' => 'Duplicate model found', 'exception' => $response ];
                 } else if (strpos($response, 'Existing') !== false) {
-                    $response = [ 'message' => 'No model found from given ID' ];
+                    $response = [ 'message' => 'No model found from given ID', 'exception' => $response];
                 } elseif (strpos($response, 'Token expired') !== false || strpos($response, 'AuthenticationFailed') !== false) {
-                    $response = [ 'message' => 'The access token has expired' ];
+                    $response = [ 'message' => 'The access token has expired', 'exception' => $response];
                 } elseif (strpos($response, 'Unsupported Operation') !== false) {
-                    $response = ['message' => 'Model cannot be edited' ];
+                    $response = ['message' => 'Model cannot be edited', 'exception' => $response];
                 } elseif (strpos($response, 'A business validation error has occurred while processing your request') !== false) {
-                    $response = [ 'message' => 'Validation exception. Possibly duplicate ID or business error' ];
+                    $response = [ 'message' => 'Validation exception. Possibly duplicate ID or business error', 'exception' => $response];
                 } else {
-                    $response = ['message' => $response];
+                    $response = ['message' => $response, 'exception' => $response];
                 }
                 return $response;
         }
